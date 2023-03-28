26e1f9f736f731c9804a159b466bb23d4588d4e1
Palak Bhatia <palakbhatia89@gmail.com>
2023-03-28 07:08:47 +0000

    Challenge 3 Solved

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -1,5 +1,16 @@
 -- Create reports that will be used to make three menus.
 
 -- Create a report with all the items sorted by price (lowest to highest).
+Select Type, Name, Price, Description
+From Dishes
+Order By Price;
 -- Create a report showing appetizers and beverages.
+Select Type, Name, Price, Description
+From Dishes
+Where Type in ('Appetizer','Beverage')
+Order By Type;
 -- Create a report with all items except beverages.
+Select Type, Name, Price, Description
+From Dishes
+Where Type Not IN ('Beverage')
+Order By Type;
