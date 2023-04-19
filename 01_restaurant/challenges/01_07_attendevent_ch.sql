197d3d3294a44c6376511a45fecabf6bee42b967
Palak Bhatia <palakbhatia89@gmail.com>
2023-04-19 01:13:47 +0000

    07 solved

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,12 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+
+select customerid as id
+from customers where email in ('atapley2j@kinetecoinc.com')
+;
+
+insert into AnniversaryAttendees
+values(92, 3);
+
+select * from AnniversaryAttendees
