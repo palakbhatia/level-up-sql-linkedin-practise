b9d3625207895e63f2648d69a0fc6a03ad328f42
Palak Bhatia <palakbhatia89@gmail.com>
2023-04-19 00:50:07 +0000

    05 and 06 solved

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,11 @@
 
 -- Taylor Jenkins, of 27170 6th Ave., Washington, DC,
 -- has moved to 74 Pine St., New York, NY.
+update Customers
+set Address = '74 Pine St.', City = 'New York', State = 'NY'
+where firstname = 'Taylor'
+and lastname = 'Jenkins'
+and City = 'Washington'
+and Address = '27170 6th Ave.'
+and State = 'DC';
+
--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,9 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+delete from reservations
+where customerId In (select customerId 
+                      from customers 
+                      where firstname IN ('Norby'))
+and date >= '2022-07-24'
