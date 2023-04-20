c73d63c0d3a8ae4c25a1567326bbdb0e01462711
Palak Bhatia <palakbhatia89@gmail.com>
2023-04-20 22:10:06 +0000

    07_updated

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -9,8 +9,3 @@ where published between 1890 and 1899
 and bookid not in (select bookid from loans where returneddate is NULL)
 group by 1
 order by title
-
-select *
-from loans 
-where bookid in 
-  (select bookid from books where barcode in (1291355714, 2054382275,2119519594)) 
