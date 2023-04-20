c46ea223b0d976d2374c8d85f89f4ab9d6b76df9
Palak Bhatia <palakbhatia89@gmail.com>
2023-04-20 22:14:11 +0000

    08_solved

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -5,3 +5,16 @@
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+select published, count(distinct title)
+from books
+group by 1
+order by 1;
+
+select title, count(loanid)
+from loans as l 
+left join books as b 
+on b.bookid = l.BookID
+group by 1
+order by 2 desc
+limit 5;
