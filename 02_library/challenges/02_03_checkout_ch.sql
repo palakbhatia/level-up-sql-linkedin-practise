1d68d01f7883b397c707626e7501bce4b2a66721
Palak Bhatia <palakbhatia89@gmail.com>
2023-04-20 21:47:55 +0000

    03_solved

--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -3,3 +3,17 @@
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+
+insert into loans(bookid, patronid, loandate, duedate)
+values((select bookid from books where barcode = 2855934983), 
+      (select patronid from patrons where email in ('jvaan@wisdompets.com')), 
+      '2022-08-25','2022-09-08');
+insert into loans(bookid, patronid, loandate, duedate)
+values((select bookid from books where barcode = 4043822646), 
+      (select patronid from patrons where email in ('jvaan@wisdompets.com')),
+       '2022-08-25','2022-09-08');
+
+select *
+from Loans
+order by loanid DESC
+        
