66229f8c5584f85c0c71b99c36b65272903b1dad
Palak Bhatia <palakbhatia89@gmail.com>
2023-04-20 21:39:49 +0000

    02_solved

--- 02_library/challenges/02_02_addbook_ch.sql
+++ 02_library/challenges/02_02_addbook_ch.sql
@@ -9,3 +9,12 @@
 -- Author: Jonathan Swift
 -- Published: 1729
 -- ID Number: 4899254401
+
+insert into books(Title, Author, Published, Barcode)
+Values('Dracula','Bram Stoker',1897, 4819277482);
+
+insert into books(Title, Author, Published, Barcode)
+Values('Gulliver’s Travels into Several Remote Nations of the World','Jonathan Swift',1729, 4899254401);
+
+select * from Books
+where title in ('Dracula', 'Gulliver’s Travels into Several Remote Nations of the World')
