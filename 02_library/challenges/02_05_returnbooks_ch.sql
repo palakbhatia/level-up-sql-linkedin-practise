5562f4f0568d27c218a00c7d183e2fc04c525a9d
Palak Bhatia <palakbhatia89@gmail.com>
2023-04-20 21:56:08 +0000

    05_solved

--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -4,3 +4,18 @@
 -- Book 1: 6435968624
 -- Book 2: 5677520613
 -- Book 3: 8730298424
+
+update loans
+set returneddate = '2022-07-05'
+where bookid in (select bookid from books where barcode = 6435968624)
+and returneddate is NULL;
+
+update loans
+set returneddate = '2022-07-05'
+where bookid in (select bookid from books where barcode = 5677520613)
+and returneddate is NULL;
+
+update loans
+set returneddate = '2022-07-05'
+where bookid in (select bookid from books where barcode = 8730298424)
+and returneddate is NULL
