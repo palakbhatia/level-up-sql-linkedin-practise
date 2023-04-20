c768a2d4546df57a59bf4a916bf95b84a7d1b501
Palak Bhatia <palakbhatia89@gmail.com>
2023-04-20 22:09:44 +0000

    06_07_solved

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,8 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+select firstname, lastname, email, count(loanid)
+from patrons as p 
+left join loans as l 
+on l.patronid = p.patronid
+group by 1,2,3
+order by 4;
--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,15 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+
+select title, author, published, count(barcode)
+from books as b 
+where published between 1890 and 1899
+and bookid not in (select bookid from loans where returneddate is NULL)
+group by 1
+order by title
+
+select *
+from loans 
+where bookid in 
+  (select bookid from books where barcode in (1291355714, 2054382275,2119519594)) 
