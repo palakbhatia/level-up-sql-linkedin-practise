c344bf6fdde25f264b57978136940d79986d8cb7
Palak Bhatia <palakbhatia89@gmail.com>
2023-04-20 18:45:04 +0000

    12_solved

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,10 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+select firstname, lastname, email, count(orderid)
+from orders
+left join customers
+on customers.customerid = orders.customerid
+group by 1,2,3
+order by 4 desc;
